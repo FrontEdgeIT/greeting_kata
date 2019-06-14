772cae9da6277269ebf6efa4e6abc8701c36218f
Fredrik Lunde <fredrik.lunde@frontedgeit.se>
2019-06-14 18:02:16 +0200

    requirement 1,2,3,4,5

--- src/greeting.js
+++ src/greeting.js
@@ -1,3 +1,24 @@
 export default function HelloWorld() {
   return "Hello World";
 }
+
+/** @param {string | string[]} name */
+export function greet(name) {
+  if (name === null) {
+    return "Hello, my friend";
+  }
+  if (name instanceof Array) {
+    if (name.length === 2) {
+      return `Hello, ${name[0]} and ${name[1]}`;
+    }
+    if (name.length > 2) {
+      const last = name[name.length - 1];
+      const init = name.slice(0, name.length - 1);
+      return `Hello, ${init.join(", ")}, and ${last}`;
+    }
+  }
+  if (name === name.toLocaleUpperCase()) {
+    return `HELLO, ${name}`;
+  }
+  return `Hello, ${name}`;
+}
--- src/greeting.test.js
+++ src/greeting.test.js
@@ -1,6 +1,26 @@
-import HelloWorld from "./greeting";
+import { greet } from "./greeting";
 
 /* global it expect */
-it("returns hello world", () => {
-  expect(HelloWorld()).toEqual("Hello World");
+it("returns customized greeting depending on input", () => {
+  expect(greet("Bob")).toEqual(`Hello, Bob`);
+});
+
+it("return Hello, My Friend when input is null", () => {
+  expect(greet(null)).toEqual(`Hello, my friend`);
+});
+
+it("returns upper case string when input is uppercase", () => {
+  expect(greet("JIMMY")).toEqual(`HELLO, JIMMY`);
+});
+
+it("returns greeting with both names if input is array", () => {
+  expect(greet(["Bob", "Jane"])).toEqual(`Hello, Bob and Jane`);
+});
+
+it("returns all names in the greeting when input is array bigger than two", () => {
+  expect(greet(["Bob", "Jane", "Jill"])).toEqual(`Hello, Bob, Jane, and Jill`);
+});
+
+it("p", () => {
+  expect(greet(["Bob", "Jane", "Jill"])).toEqual(`Hello, Bob, Jane, and Jill`);
 });
